a94a83eb3c1b07bd19d77db47e8fac20c24a8fb8
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-03-07 11:08:01 -0800

    prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,3 @@ while number < 89:
     if number % 10 == 0:
         number += (number // 10) + 1
     print('{:0>2d}'.format(number), end=', ' if number < 89 else '\n')
-for number in range(0, 99):
-    print('{:0>2d}, '.format(number), end='')
-print(99)

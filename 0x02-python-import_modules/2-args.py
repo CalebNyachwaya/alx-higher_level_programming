5c7851af5d622134f078bbdfa20cb50237c132a1
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-03-12 22:58:36 -0700

    prints no. and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    import sys
     # Get all the command-line arguments except the script name
     args = sys.argv[1:]
 
@@ -15,8 +15,8 @@ if __name__ == "__main__":
 
     # Print the list of arguments
     if arg_count > 0:
-        for i, arg in enumerate(args, 1):
-            print(i, ":", arg)
+        for i in range(1,arg_count + 1):
+            print('{:d}: {:s}'.format(i, sys.argv[i]))
 
     # If no arguments were passed, print a period and a newline
     else:

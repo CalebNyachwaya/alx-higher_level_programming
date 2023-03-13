96add85e0c9ad01685ad3129e75a17a52b830457
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-03-12 23:01:40 -0700

    fix checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    # Get all the command-line arguments except the script name
-    args = sys.argv[1:]
-
-    # Print the number of arguments
-    arg_count = len(args)
-    if arg_count == 0:
+    count = len(sys.argv)
+    if (count == 1):
         print("0 arguments.")
-    elif arg_count == 1:
+    elif (count == 2):
         print("1 argument:")
     else:
-        print(arg_count, "Arguments:")
-
-    # Print the list of arguments
-    if arg_count > 0:
-        for i in range(1,arg_count + 1):
-            print('{:d}: {:s}'.format(i, sys.argv[i]))
-
-    # If no arguments were passed, print a period and a newline
-    else:
-        pass
+        print('{:d} arguments:'.format(count - 1))
+    for arguments in range(1, count):
+        if (count > 1):
+            print('{:d}: {:s}'.format(arguments, sys.argv[arguments]))

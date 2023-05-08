eb665f77147d4e060bf49a645dbc5b18f7e4003f
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-05-08 10:09:37 -0700

    Use f-strings

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # print 3 times the value of str, also the 9 first characters of str
-
 str = "Holberton School"
-print(str+str+str+"\n"+str[:10])
+print(f"{str*3}\n"+str[:10])    # or print(str+str+str+"\n"+str[:10])

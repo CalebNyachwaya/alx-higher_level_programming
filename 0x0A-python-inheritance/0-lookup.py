755bbf7971e68dff62bcc6dc4d25bfb17b76c0af
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-06-12 09:22:58 -0700

    Fix pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-#0-lookup.py
+# 0-lookup.py
 """Defines an object attribute"""
+
+
 def lookup(obj):
     """function that returns available attributes and methods"""
     return dir(obj)

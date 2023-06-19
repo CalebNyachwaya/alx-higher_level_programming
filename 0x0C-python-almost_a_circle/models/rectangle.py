8f42287082ca0cd445c1a0c84f0e449942d07aa8
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-06-19 11:02:37 -0700

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # rectangle.py
-""" module for class that inherites from base class """
+""" module imports module base """
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ inherits from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructor"""
         self.__width = width
         self.__height = height
         self.__x = x
@@ -14,33 +16,41 @@ class Rectangle(Base):
         super().__init__(id)
 
     @property
+    """Getter method for width attribute"""
     def width(self):
         return self.__width
 
     @width.setter
+    """Setter method for width attribute"""
     def width(self, value):
         self.__width = value
 
     @property
+    """Setter method for height attribute"""
     def height(self):
         return self.__height
 
     @height.setter
+    """Setter method for height attribute"""
     def height(self, value):
         self.__height = value
 
     @property
+    """Getter method for x attribute"""
     def x(self):
         self.__x = x
 
     @x.setter
+    """Setter method for x attribute"""
     def x(self, value):
         self.__x = value
 
     @property
+    """Getter method for y attribute"""
     def y(self):
         return self.__y
 
     @y.setter
+    """Setter method for y attribute"""
     def y(self, value):
         self.__y = value

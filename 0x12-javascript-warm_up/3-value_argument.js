26e9f9a49ba3ee94e037a1c00b36f0b8379b6ca3
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-14 11:33:37 -0700

    Update comment

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* access the commandline and remove the first element */
+/* access the commandline and remove the first 2 elements */
 const args = process.argv.slice(2);
 
 if (args[0] !== undefined) {

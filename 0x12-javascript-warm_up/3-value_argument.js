78110203efbd886b669b83dac8a85e54b58edc97
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-14 11:26:04 -0700

    Add 3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* access the commandline and remove the first element */
+const args = process.argv.slice(2);
+
+if (args[0] !== undefined) {
+  console.log(args[0]);
+} else {
+  console.log('No argument');
+}

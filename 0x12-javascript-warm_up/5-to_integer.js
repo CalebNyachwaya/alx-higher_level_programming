d2fabaf9473fea5f4e1b9408e98b20401a5d23f5
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-14 22:43:12 -0700

    Fix Semi-Standard Style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* access the commandline and remove the first 2 elements */
 const args = process.argv.slice(2);
-//convert string to value
+// convert string to value
 const value = parseInt(args[0]);
 if (isNaN(value)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + value)
+  console.log('My number: ' + value);
 }

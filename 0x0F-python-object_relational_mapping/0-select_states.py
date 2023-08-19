6816aee70e0304d9cf09c2f4a88d7f1013462046
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-19 06:11:34 -0700

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,17 @@
 import MySQLdb
 import sys
 
-# define commandline arguments
-uName = sys.argv[1]
-paswd = sys.argv[2]
-dtbase = sys.argv[3]
+if __name__ == "__main__":
+    # define commandline arguments
+    uName = sys.argv[1]
+    paswd = sys.argv[2]
+    dtbase = sys.argv[3]
+    
+    db = MySQLdb.connect(user=uName, password=paswd, database=dtbase)
+    cur = db.cursor()
+    # execute SQL query using execute() method
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-db = MySQLdb.connect(user=uName, password=paswd, database=dtbase)
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY id")
-for state in cur.fetchall():
-    print(state)
+    # fetch a single row using fetchall()
+    for state in cur.fetchall():
+        print(state)

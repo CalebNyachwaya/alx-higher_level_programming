dc2633bfc23fa4330c78cad115d1db687001a6d7
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-08-19 06:38:32 -0700

    Add documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Module to connect to the database and print the data from the
+    Table: States.
+"""
+
 import MySQLdb
 import sys
 
@@ -8,8 +12,12 @@ if __name__ == "__main__":
     paswd = sys.argv[2]
     dtbase = sys.argv[3]
     
+    # Open database connection
     db = MySQLdb.connect(port=3306, user=uName, passwd=paswd, db=dtbase)
+
+    # access to database interface
     cur = db.cursor()
+
     # execute SQL query using execute() method
     cur.execute("SELECT * FROM states ORDER BY id")
 

6d691149a14e1090880953fa2d722af4bbac59c9
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-09-01 16:48:47 -0700

    Add comments for readability

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+script that takes 2 arguments in order to list 10 commits (from the most
+recent to oldest) of the repository "rails" by the user "rails".
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
 import sys
 import requests
 

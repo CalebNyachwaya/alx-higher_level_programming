d6f374393504f8e797f6d1e447a78016dc8b1cdc
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-10-09 09:29:51 -0700

    add 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var headerElement = document.querySelector('header');
+
+  // Update the text color to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});

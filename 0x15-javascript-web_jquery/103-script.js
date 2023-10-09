ef363f8ba30706a8a55b4f4c1b1dbaef113bc5a1
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-10-09 09:41:58 -0700

    Add comments for better readability

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,18 +1,23 @@
-$('document').ready(function () {
+$(document).ready(function () {
     $('INPUT#btn_translate').click(translate);
+
     $('INPUT#language_code').focus(function () {
-	$(this).keydown(function (e) {
-	    if (e.keyCode === 13) {
-		translate();
-	    }
-	});
+        // Attach a keydown event handler to the input field
+        $(this).keydown(function (e) {
+            // Check if the Enter key (keyCode 13) is pressed
+            if (e.keyCode === 13) {
+                translate();
+            }
+        });
     });
 });
 
 function translate () {
     const url = 'https://www.fourtonfish.com/hellosalut/?';
-    $.get(url + $.param({ lang: $('INPUT#language_code').val() }),
-	  function (data) {
-	$('DIV#hello').html(data.hello);
+
+    // Send a GET request to the API with the language code as a parameter
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+        // Update the content of the element with ID "hello" with the translated text
+        $('DIV#hello').html(data.hello);
     });
 }
